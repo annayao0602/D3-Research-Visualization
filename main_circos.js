6cff4d0acfc96e3a73247652bca28509eae098f6
AnnaYao <annayao@Annas-MacBook-Air-6.local>
2025-08-28 17:15:22 -0400

    changes to layout

--- main_circos.js
+++ main_circos.js
@@ -4,7 +4,7 @@ let myCircosChart;
 var CircosOptions = {
     margin: {top: 200, right: 200, bottom: 200, left: 200},
     maxValue: 10000,
-    labels: true
+    labels: false
 }
 
 function loadDataAndDraw() {
