4e06b1ab651b7b8c1e8b8d9efac7f8e6f018ba0c
AnnaYao <annayao@Annas-MacBook-Air-6.local>
2025-09-04 10:25:13 -0400

    connected pages with buttons

--- bar.js
+++ bar.js
@@ -9,63 +9,63 @@ var svg = d3.select("#barchart")
     .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        d3.csv("https://raw.githubusercontent.com/annayao0602/D3-Research-Visualization/main/numpub.csv", function(data) {
-            data.forEach(function(d) {
-                d.numpub = +d.numpub;
-            });
+d3.csv("https://raw.githubusercontent.com/annayao0602/D3-Research-Visualization/main/numpub.csv", function(data) {
+    data.forEach(function(d) {
+        d.numpub = +d.numpub;
+    });
 
-            data.sort((a,b) => b.numpub - a.numpub);
+    data.sort((a,b) => b.numpub - a.numpub);
 
-            var topData = data.slice(0,5);
+    var topData = data.slice(0,5);
 
-            const allDomains = [...new Set(data.map(d => d.Domain))];
-            const domainColor = d3.scaleOrdinal()
-                .domain(allDomains)
-                .range(["#003f5c", "#bc5090", "#58508d", "#ffa600"]);
+    const allDomains = [...new Set(data.map(d => d.Domain))];
+    const domainColor = d3.scaleOrdinal()
+        .domain(allDomains)
+        .range(["#003f5c", "#bc5090", "#ffa600", "#58508d"]);
             
-            const tooltip = d3.select("body").append("div").attr("class", "tooltip");
+    const tooltip = d3.select("body").append("div").attr("class", "tooltip");
 
-            var x = d3.scaleLinear()
-                .domain([0, d3.max(topData, d => d.numpub)])
-                .range([ 0, width]);
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(topData, d => d.numpub)])
+        .range([ 0, width]);
 
-            var y = d3.scaleBand()
-                .range([ 0, height ])
-                .domain(topData.map(function(d) { return d.Field; }))
-                .padding(.1);
-            svg.selectAll(".bar-label")
-                .data(topData)
-                .enter()
-                .append("text")
-                .attr("class", "bar-label")
-                .attr("x", d => x(d.numpub) + 10) 
-                .attr("y", d => y(d.Field) + y.bandwidth() / 2)
-                .attr("dy", "0.35em") 
-                .style("font-family", "Jura")
-                .style("font-size", "13px")
-                .text(d => d.Field);
+    var y = d3.scaleBand()
+        .range([ 0, height ])
+        .domain(topData.map(function(d) { return d.Field; }))
+        .padding(.1);
+    svg.selectAll(".bar-label")
+        .data(topData)
+        .enter()
+        .append("text")
+        .attr("class", "bar-label")
+        .attr("x", d => x(d.numpub) + 10) 
+        .attr("y", d => y(d.Field) + y.bandwidth() / 2)
+        .attr("dy", "0.35em") 
+        .style("font-family", "Jura")
+        .style("font-size", "13px")
+        .text(d => d.Field);
 
-            svg.selectAll("myRect")
-                .data(topData)
-                .enter()
-                .append("rect")
-                .attr("x", x(0) )
-                .attr("y", function(d) { return y(d.Field); })
-                .attr("width", function(d) { return x(d.numpub); })
-                .attr("height", y.bandwidth() )
-                .attr("fill", d => domainColor(d.Domain))
-                .attr("fill-opacity", 0.7)
-                .on("mouseover", function(d) {
-                    tooltip.style("opacity", 1);
-                    tooltip.html(`Value: ${parseInt(d.numpub).toLocaleString()}`);
-                    d3.select(this).attr("fill-opacity", 1);
-                })
-                .on("mousemove", function(event) {
-                    tooltip.style("left", (d3.event.pageX - 105) + "px")
-                       .style("top", (d3.event.pageY - 25) + "px");
-                })
-                .on("mouseout", function(d) {
-                    tooltip.style("opacity", 0);
-                    d3.select(this).attr("fill-opacity", 0.7);
-                })
-            })
+    svg.selectAll("myRect")
+        .data(topData)
+        .enter()
+        .append("rect")
+        .attr("x", x(0) )
+        .attr("y", function(d) { return y(d.Field); })
+        .attr("width", function(d) { return x(d.numpub); })
+        .attr("height", y.bandwidth() )
+        .attr("fill", d => domainColor(d.Domain))
+        .attr("fill-opacity", 0.7)
+        .on("mouseover", function(d) {
+            tooltip.style("opacity", 1);
+            tooltip.html(`Value: ${parseInt(d.numpub).toLocaleString()}`);
+            d3.select(this).attr("fill-opacity", 1);
+        })
+        .on("mousemove", function(event) {
+            tooltip.style("left", (d3.event.pageX - 105) + "px")
+                .style("top", (d3.event.pageY - 25) + "px");
+        })
+        .on("mouseout", function(d) {
+            tooltip.style("opacity", 0);
+            d3.select(this).attr("fill-opacity", 0.7);
+        })
+    })
