3c6dc8a62845ab6f56db4d08de17019a79f160e7
AnnaYao <annayao@Annas-MacBook-Air-6.local>
2025-09-11 12:28:02 -0400

    pseudocode written for bar chart

---
+++ multibar.js
@@ -0,0 +1,32 @@
+
+//-----MULTIBAR CHART-----
+
+//create function multibar() - param: selector, data, options
+
+//create default configurations (width, height, max val)
+
+//make initial axes
+
+/* update function: updates after data source changes
+    d.value = numpub or numpub_total
+    d.name = Field if there, if not then domain, if not the group
+
+    sort data by descending order
+
+    get domain colors
+
+    tooltip, x, y variables
+
+    ENTER EXIT UPDATE FOR:
+     -make labels 
+
+     -create bars
+
+    return update
+
+*/
+
+//call update
+
+//---HTML SCRIPT---
+//load function: loads in datasets and calls multibar function to draw
