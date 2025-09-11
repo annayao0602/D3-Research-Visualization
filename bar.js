c8baeac6360f2597ea392c07000a0c70600c2efd
AnnaYao <annayao@Annas-MacBook-Air-6.local>
2025-09-11 11:43:36 -0400

    added group domain data

--- bar.js
+++ bar.js
@@ -9,55 +9,53 @@ var svg = d3.select("#barchart")
     .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("https://raw.githubusercontent.com/annayao0602/D3-Research-Visualization/main/numpub.csv", function(data) {
+d3.csv("https://raw.githubusercontent.com/annayao0602/D3-Research-Visualization/main/numpub_domain.csv", function(data) {
     data.forEach(function(d) {
-        d.numpub = +d.numpub;
+        d.numpub_total = +d.numpub_total;
     });
 
-    data.sort((a,b) => b.numpub - a.numpub);
+    data.sort((a,b) => b.numpub_total - a.numpub_total);
 
-    var topData = data.slice(0,5);
-
-    const allDomains = [...new Set(data.map(d => d.Domain))];
+    const allDomains = [...new Set(data.map(d => d.Domain_total))];
     const domainColor = d3.scaleOrdinal()
         .domain(allDomains)
-        .range([ "#bc5090", "#003f5c", "#ffa600", "#58508d"]);
+        .range([ "#58508d","#003f5c","#ffa600","#bc5090"]);
             
     const tooltip = d3.select("body").append("div").attr("class", "tooltip");
 
     var x = d3.scaleLinear()
-        .domain([0, d3.max(topData, d => d.numpub)])
+        .domain([0, d3.max(data, d => d.numpub_total)])
         .range([ 0, width]);
 
     var y = d3.scaleBand()
         .range([ 0, height ])
-        .domain(topData.map(function(d) { return d.Field; }))
+        .domain(data.map(function(d) { return d.Domain_total; }))
         .padding(.1);
     svg.selectAll(".bar-label")
-        .data(topData)
+        .data(data)
         .enter()
         .append("text")
         .attr("class", "bar-label")
-        .attr("x", d => x(d.numpub) + 10) 
-        .attr("y", d => y(d.Field) + y.bandwidth() / 2)
+        .attr("x", d => x(d.numpub_total) + 10) 
+        .attr("y", d => y(d.Domain_total) + y.bandwidth() / 2)
         .attr("dy", "0.35em") 
         .style("font-family", "Jura")
         .style("font-size", "13px")
-        .text(d => d.Field);
+        .text(d => d.Domain_total);
 
     svg.selectAll("myRect")
-        .data(topData)
+        .data(data)
         .enter()
         .append("rect")
         .attr("x", x(0) )
-        .attr("y", function(d) { return y(d.Field); })
-        .attr("width", function(d) { return x(d.numpub); })
+        .attr("y", function(d) { return y(d.Domain_total); })
+        .attr("width", function(d) { return x(d.numpub_total); })
         .attr("height", y.bandwidth() )
-        .attr("fill", d => domainColor(d.Domain))
+        .attr("fill", d => domainColor(d.Domain_total))
         .attr("fill-opacity", 0.7)
         .on("mouseover", function(d) {
             tooltip.style("opacity", 1);
-            tooltip.html(`Value: ${parseInt(d.numpub).toLocaleString()}`);
+            tooltip.html(`Value: ${parseInt(d.numpub_total).toLocaleString()}`);
             d3.select(this).attr("fill-opacity", 1);
         })
         .on("mousemove", function(event) {
